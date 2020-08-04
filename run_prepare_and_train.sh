6651e84275da63188ec8af094ee4fc4ddc7bd51a
Brandon Thomas <echelon@gmail.com>
2020-08-04 02:16:18 -0400

    source python venv

--- run_prepare_and_train.sh
+++ run_prepare_and_train.sh
@@ -4,6 +4,9 @@ model_name=$1
 data_path="/home/ubuntu/data/melgan_${model_name}/"
 existing_checkpoint="/home/ubuntu/data/melgan_checkpoint.pt"
 
+# use venv context
+source python/bin/activate
+
 python preprocess.py \
   --config=config/default.yaml \
   --data_path="${data_path}"
@@ -12,5 +15,4 @@ python trainer.py \
   --config=config/default.yaml \
   --checkpoint_path="${existing_checkpoint}" \
   --name="melgan_${model_name}"
-
 
