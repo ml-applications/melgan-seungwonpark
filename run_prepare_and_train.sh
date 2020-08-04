2167fbe9bf0b9b3820bb2dd6c14bb2f35457cc7a
Brandon Thomas <echelon@gmail.com>
2020-08-04 02:20:17 -0400

    check that model name is set; strict eval

--- run_prepare_and_train.sh
+++ run_prepare_and_train.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 model_name=$1
+
+if [ -z "$model_name" ]; then
+  echo "Model name was not set."
+  exit
+fi
+
 data_path="/home/ubuntu/data/melgan_${model_name}/"
 existing_checkpoint="/home/ubuntu/data/melgan_checkpoint.pt"
 
