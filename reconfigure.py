04b87e3ff1a2702c7b9b96f3586aede8d15b11c0
Brandon Thomas <echelon@gmail.com>
2020-08-23 21:37:48 -0400

    Update 'reconfigure.py' to run locally

--- reconfigure.py
+++ reconfigure.py
@@ -3,9 +3,14 @@
 from ruamel.yaml import YAML
 import argparse
 import os
+import socket
 
 DEFAULT_CONFIG_FILE = 'config/default.yaml'
-DEFAULT_DATA_DIRECTORY = '/home/ubuntu/data/'
+
+# Sometimes I train on my local 'halide' GPU worker,
+# other times I rent instances from LambdaLabs.
+DEFAULT_DATA_DIRECTORY_LOCAL = '/home/bt/datasets/'
+DEFAULT_DATA_DIRECTORY_REMOTE = '/home/ubuntu/data/'
 
 def read_yaml(filename):
   contents = None
@@ -20,6 +25,13 @@ def write_yaml(filename, config_dict):
   with open(filename, 'w') as f:
     yaml.dump_all(config_dict, f)
 
+def get_data_directory():
+  hostname = socket.gethostname()
+  if hostname == 'halide':
+    return DEFAULT_DATA_DIRECTORY_LOCAL
+  else:
+    return DEFAULT_DATA_DIRECTORY_REMOTE
+
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--speaker_dir', type=str, required=True,
@@ -28,8 +40,12 @@ def main():
 
   print('Speaker directory: {}'.format(args.speaker_dir))
 
+  data_directory = get_data_directory()
+
+  print('Data directory: {}'.format(data_directory))
+
   speaker_dir = 'melgan_{}'.format(args.speaker_dir)
-  speaker_dir = os.path.join(DEFAULT_DATA_DIRECTORY, speaker_dir)
+  speaker_dir = os.path.join(data_directory, speaker_dir)
 
   training_directory = os.path.join(speaker_dir, 'training')
   validation_directory = os.path.join(speaker_dir, 'validation')
